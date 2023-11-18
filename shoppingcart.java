56347fc66934ac8f53f2f8d7b5ba6c8728be8a24
Anushree2812 <151332157+Anushree2812@users.noreply.github.com>
2023-11-18 13:11:30 +0530

    Create shoppingcart.java

---
+++ shoppingcart.java
@@ -0,0 +1,27 @@
+int leather wallet (int  price rs,int gst,int quantity )
+{   
+   int price=1100;
+   int gst=18;
+   int quantity=1;
+   m2=gst*p/100;
+   return m2;
+ 
+voidgetcalculatefor total basket value()
+{
+ public void  leather wallet(1100,18,1);
+  {
+  system.out.println("total gst paid to leather wallet");
+  }
+  public void umbrella(900,1,4);
+  {
+   system.out.println("total gst paid to umbrella");
+  }
+  public void cigarette(200,28,3);
+  {
+	system.out.println("total gst paid to cigarette");
+   }
+  public void honey (100,0,2);
+  {
+	system.out.println("total gst paid to honey");
+   }
+   
